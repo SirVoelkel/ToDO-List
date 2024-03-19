2b937d3a8176c20931d4aea4502aa503689344c4
Logan_Völkel <Logan_you_dont_know@Fuckoff.com>
2024-03-19 09:37:19 +0100

    todoItem-works

--- add.js
+++ add.js
@@ -23,13 +23,26 @@ function addTodo(){
 function updateTodoList(){
     todoListUL.innerHTML = "";
     allToDos.forEach((todo, todoIndex) => {
-        todoItem = createToDoItem(todo, todoIndex);
+        todoItem = createTodoItem(todo, todoIndex);
         todoListUL.append(todoItem);
     })
 }
 
-function createToDoItem(todo){
+function createTodoItem(todo, todoIndex){
+    const todoId = "todo-"+todoIndex;
     const todoLI = document.createElement("li");
-    todoLI.innerText = todo;
+    todoLI.className = "todo"
+    todoLI.innerHTML = /*html*/ `
+        <input id="${todoId}" type="checkbox">
+        <label class="custom-checkbox" for="${todoId}" >
+            <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
+        </label>
+        <label class="todo-text" for="${todoId}">
+            ${todo} 
+        </label>
+        <button class="delete-button">
+            <svg fill="var(--accent-color)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+        </button>
+    `
     return todoLI;
-};
+};  
