d5589233ba3762254246b1a3431348b8fac339b4
Logan_Völkel <Logan_you_dont_know@Fuckoff.com>
2024-03-19 11:03:35 +0100

    it-works

--- add.js
+++ add.js
@@ -2,7 +2,8 @@ const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoListUL = document.getElementById('todo-list');
 
-let allToDos = [];
+let allToDos = getTodos();
+updateTodoList();
 
 todoForm.addEventListener('submit', function(e){
     e.preventDefault();
@@ -14,10 +15,9 @@ function addTodo(){
     if(todoText.length > 0){
         allToDos.push(todoText);
         updateTodoList();
+        saveTodos();
         todoInput.value = '';
     }
-
-    console.log(allToDos);
 }
 
 function updateTodoList(){
@@ -44,5 +44,27 @@ function createTodoItem(todo, todoIndex){
             <svg fill="var(--accent-color)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
         </button>
     `
+    const deleteButton = todoLI.querySelector(".delete-button");
+    deleteButton.addEventListener("click", () => {
+        deleteTodoItem(todoIndex);
+    })
+
     return todoLI;
-};  
+};
+
+function deleteTodoItem(todoIndex){
+    allToDos = allToDos.filter((_, i) => i !== todoIndex);
+    saveTodos();
+    updateTodoList();
+}
+
+// Local Storage Function's
+function saveTodos(){
+    const todosJson = JSON.stringify(allToDos);
+    localStorage.setItem("todos", todosJson)
+}
+
+function getTodos(){
+    const todos = localStorage.getItem("todos") || "[]"
+    return JSON.parse(todos);
+}
